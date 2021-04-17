1b36b3ef172df2a98145c4f2bc7b899db44e322e
Eric Pederson <epederson@denniskirk.com>
2021-04-17 12:11:42 -0500

    Sql table structure

--- tables.sql
+++ tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE golfer
 (
-    id      int auto_increment not null,
+    id          int auto_increment not null,
     first_name  varchar(128)       not null,
     last_name   varchar(128)       not null,
     middle_name varchar(128),
@@ -10,18 +10,32 @@ CREATE TABLE golfer
     zip         varchar(128),
     home_phone  varchar(128),
     work_phone  varchar(128),
-    status      int                         default 1,
-    paid        tinyint                     default 0,
-    new         tinyint                     default 0,
     notes       varchar(256),
     date_added  DATETIME           not null default NOW(),
     primary key (id)
 );
 
+CREATE TABLE golfer_status
+(
+    golfer_id int          not null,
+    status_id varchar(128) not null,
+    updated   datetime     not null default NOW(),
+    primary key (golfer_id, status_id),
+    FOREIGN KEY (golfer_id) REFERENCES golfer (id),
+    FOREIGN KEY (status_id) REFERENCES status (name)
+);
+
+CREATE TABLE status
+(
+    name        varchar(128) not null,
+    description varchar(256),
+    primary key (name)
+);
+
 CREATE TABLE team_member
 (
-    team_id int not null,
-    golfer_id  int not null,
+    team_id   int not null,
+    golfer_id int not null,
     primary key (team_id, golfer_id),
     FOREIGN KEY (team_id) REFERENCES team (id),
     FOREIGN KEY (golfer_id) REFERENCES golfer (id)
@@ -30,7 +44,7 @@ CREATE TABLE team_member
 CREATE TABLE team
 (
     id          int auto_increment not null,
-    league_id int not null,
+    league_id   int                not null,
     name        varchar(128),
     description varchar(128),
     primary key (id),
@@ -39,22 +53,22 @@ CREATE TABLE team
 
 CREATE TABLE team_flight
 (
-    team_id int not null,
-    league_id int not null,
-    year YEAR not null,
-    PRIMARY KEY (team_id, league_id, year),
+    team_id   int not null,
+    flight_id int not null,
+    PRIMARY KEY (team_id, flight_id),
     FOREIGN KEY (team_id) references team (id),
-    FOREIGN KEY (league_id, year) references flight (league_id, year)
+    FOREIGN KEY (flight_id) references flight (id)
 );
 
 CREATE TABLE flight
 (
+    id        int  not null auto_increment,
     league_id int  not null,
-    year      YEAR not null,
     start     TIME not null,
     end       TIME not null,
-    primary key (league_id, year, start, end),
-    FOREIGN KEY (league_id, year) references season (league_id, year)
+    primary key (id),
+    FOREIGN KEY (league_id) references league (id),
+    UNIQUE (league_id, start, end)
 );
 
 CREATE TABLE league
@@ -66,31 +80,43 @@ CREATE TABLE league
 
 CREATE TABLE season
 (
-    year      YEAR not null,
-    league_id int  not null,
-    course_id int  not null,
-    primary key (year, league_id),
+    id        int auto_increment not null,
+    year      YEAR               not null,
+    league_id int                not null,
+    course_id int                not null,
+    primary key (id),
     FOREIGN KEY (league_id) REFERENCES league (id),
-    FOREIGN KEY (course_id) REFERENCES course (id)
+    FOREIGN KEY (course_id) REFERENCES course (id),
+    UNIQUE (year, league_id, course_id)
 );
 
 CREATE TABLE event
 (
-    year    YEAR not null,
-    day     DATE not null,
-    league_id int  not null,
+    id         int          not null auto_increment,
+    season_id  int          not null,
+    day        DATE         not null,
     event_type VARCHAR(128) not null,
-    notes VARCHAR(256),
-    PRIMARY KEY (year, day),
-    FOREIGN KEY (league_id) REFERENCES league (id),
+    notes      VARCHAR(256),
+    PRIMARY KEY (id),
+    FOREIGN KEY (season_id) REFERENCES season (id),
     FOREIGN KEY (event_type) REFERENCES event_type (name)
 );
 
 CREATE TABLE event_type
 (
-    name VARCHAR(128) NOT NULL,
+    name        VARCHAR(128) NOT NULL,
     description VARCHAR(256),
     PRIMARY KEY (name)
+);
+
+
+CREATE TABLE team_event
+(
+    event_id         int          not null auto_increment,
+    team_id  int          not null,
+    PRIMARY KEY (event_id, team_id),
+    FOREIGN KEY (event_id) REFERENCES event (id),
+    FOREIGN KEY (team_id) REFERENCES team (id)
 );
 
 CREATE TABLE course
@@ -104,12 +130,13 @@ CREATE TABLE nine
 (
     course_id int          not null,
     name      varchar(256) not null,
-    primary key (course_id, name)
+    primary key (course_id, name),
+    FOREIGN KEY (course_id) REFERENCES course (id)
 );
 
 CREATE TABLE hole
 (
-    id int not null,
+    id          int          not null,
     course_id   int          not null,
     nine_name   varchar(256) not null,
     hole_number int          not null,
@@ -120,53 +147,50 @@ CREATE TABLE hole
 
 CREATE TABLE hole_handicap
 (
-    hole_id int not null,
-    created DATETIME not null DEFAULT NOW(),
-    handicap int not null,
+    hole_id  int      not null,
+    created  DATETIME not null DEFAULT NOW(),
+    handicap int      not null,
     PRIMARY KEY (hole_id, created),
     FOREIGN KEY (hole_id) REFERENCES hole (id)
 );
 
 CREATE TABLE hole_par
 (
-    hole_id int not null,
+    hole_id int      not null,
     created DATETIME not null DEFAULT NOW(),
-    par int not null,
+    par     int      not null,
     PRIMARY KEY (hole_id, created),
     FOREIGN KEY (hole_id) REFERENCES hole (id)
 );
 
 CREATE TABLE player_handicap
 (
-    id INT auto_increment not null,
-    golfer_id int not null,
-    created DATETIME NOT NULL default NOW(),
-    handicap int not null,
+    id        INT auto_increment not null,
+    golfer_id int                not null,
+    created   DATETIME           NOT NULL default NOW(),
+    handicap  int                not null,
     PRIMARY KEY (id),
     FOREIGN KEY (golfer_id) references golfer (id)
 );
 
 CREATE TABLE round
 (
-    year YEAR NOT NULL,
-    day DATE NOT NULL,
-    league_id INT not null,
+    id        int not null auto_increment,
+    event_id  int not null,
     golfer_id int not null,
-    primary key (year, day, league_id, golfer_id),
-    FOREIGN KEY (league_id, year, day) references event (league_id, year, day),
-    FOREIGN KEY (golfer_id) REFERENCES golfer (id)
+    primary key (id),
+    FOREIGN KEY (event_id) references event (id),
+    FOREIGN KEY (golfer_id) REFERENCES golfer (id),
+    UNIQUE (event_id, golfer_id)
 );
 
 CREATE TABLE score
 (
-    year YEAR NOT NULL,
-    day DATE NOT NULL,
-    league_id INT not null,
-    golfer_id int not null,
-    hole_id int not null,
-    score int not null,
-    PRIMARY KEY (year, day, league_id, golfer_id, hole_id),
-    FOREIGN KEY (year, day, league_id, golfer_id) REFERENCES round (year, day, league_id, golfer_id),
-    FOREIGN KEY (hole_id) REFERENCES hole ()
+    round_id int not null,
+    hole_id  int not null,
+    score    int not null,
+    PRIMARY KEY (round_id, hole_id),
+    FOREIGN KEY (round_id) REFERENCES round (id),
+    FOREIGN KEY (hole_id) REFERENCES hole (id)
 )
 
