fa790c28e9f5ad93aa4c956496442776c08013a4
eric <ericjohnpederson@gmail.com>
2021-06-02 14:55:36 -0500

    A better way to calculate first hole in row.

--- frontend/src/GlRound.js
+++ frontend/src/GlRound.js
@@ -35,25 +35,17 @@ export class GlRound extends LitElement {
 
     connectedCallback() {
         super.connectedCallback();
-        this.addEventListener("gl-hole-first-update", this.setLabelsOnFirstInRow.bind(this));
+        this.addEventListener("gl-hole-first-update", this._onHoleFirstUpdate.bind(this));
         this.addEventListener("gl-hole-score-change", this._onHoleScoreChange.bind(this));
     }
 
-    setLabelsOnFirstInRow(e) {
+    _onHoleFirstUpdate(e) {
         if(e.detail.hole === 9){
             // Calculate which holes are first in the row then call show label on those holes.
             const slots = [...this.shadowRoot.querySelectorAll("slot")];
             const holes = slots.map(slot => slot.assignedElements()[0]);
-            const holeContainer = this.shadowRoot.querySelector('.round-container').getBoundingClientRect();
-            const widthOfElement = holes[0].getBoundingClientRect().width;
-            const maxWidthOfLabeledHole = 100;
-            const maxHolesPerRow = Math.floor(((holeContainer.right - holeContainer.left) - (maxWidthOfLabeledHole - widthOfElement)) / widthOfElement);
-            let y = 9 + maxHolesPerRow;
-            while (y > maxHolesPerRow) {
-                y -= maxHolesPerRow;
-                const firstHoleInRow = holes[9 - y];
-                firstHoleInRow.showLabel();
-            }
+            const left = holes.sort((a,b) => { return a.getBoundingClientRect().left - b.getBoundingClientRect().left;})[0].getBoundingClientRect().left;
+            holes.forEach((hole) => {if(hole.getBoundingClientRect().left === left){hole.showLabel()}});
         }
     }
 
